f908a2e3f61b971d62115360a68ecb76d776088e
Slavka-JD <miledi777@ukr.net>
2015-02-02 04:06:18 +0200

    added Employeefixtures

--- src/AppBundle/DataFixtures/ORM/LoadData.php
+++ src/AppBundle/DataFixtures/ORM/LoadData.php
@@ -10,7 +10,7 @@ class LoadData extends DataFixtureLoader
     protected function getFixtures()
     {
         return array(
-            __DIR__ . '/fixturesAffiche.yml', __DIR__ . '/fixturesRole.yml', __DIR__ . '/fixturesPerformance.yml',
+            __DIR__ . '/fixturesAffiche.yml', __DIR__ . '/fixturesRole.yml', __DIR__ . '/fixturesPerformance.yml', __DIR__ . '/fixturesEmployee.yml',
         );
     }
 }
