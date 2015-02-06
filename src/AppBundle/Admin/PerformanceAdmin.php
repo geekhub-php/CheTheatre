a97450b178e4b5a7d7d769690056959a3f5f35de
Vladimir Gavrylov <iboved@gmail.com>
2015-02-06 15:02:01 +0200

    Added PerformanceAdmin

---
+++ src/AppBundle/Admin/PerformanceAdmin.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class PerformanceAdmin extends Admin
+{
+    protected $baseRouteName = 'AppBundle\Entity\Performance';
+    protected $baseRoutePattern = 'performance';
+    protected $datagridValues = array(
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'name',
+    );
+
+    /**
+     * @param FormMapper $formMapper
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+            ->add('description')
+            ->add('premiere')
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $datagridMapper
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+            ->add('description')
+            ->add('premiere')
+            ->add('performanceEvents')
+            ->add('roles')
+        ;
+    }
+
+    /**
+     * @param ListMapper $listMapper
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('description')
+            ->add('premiere')
+            ->add('performanceEvents')
+            ->add('roles')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+}
