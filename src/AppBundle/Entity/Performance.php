54b8c0fcfa1cafbe97be17ad73240472843bc05b
Vladimir Gavrylov <iboved@gmail.com>
2015-02-06 18:54:03 +0200

    Changed addRole method

--- src/AppBundle/Entity/Performance.php
+++ src/AppBundle/Entity/Performance.php
@@ -233,6 +233,7 @@ class Performance
      */
     public function addRole(\AppBundle\Entity\Role $role)
     {
+        $role->setPerformance($this);
         $this->roles[] = $role;
 
         return $this;
