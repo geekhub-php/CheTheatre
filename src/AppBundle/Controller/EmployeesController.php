0f20a16492093886f18e67fe2ec816bd3994cbb8
Sergey Polischuk <spolischook@gmail.com>
2015-02-09 21:28:20 +0700

    Updated 404 error api doc description

--- src/AppBundle/Controller/EmployeesController.php
+++ src/AppBundle/Controller/EmployeesController.php
@@ -24,7 +24,7 @@ class EmployeesController extends Controller
      *  description="Returns a collection of Employees",
      *  statusCodes={
      *      200="Returned when successful",
-     *      404="Returned when the entity is not found",
+     *      404="Returned when the entities with given limit and offset is not found",
      *  },
      *  output = "array<AppBundle\Entity\Employee>"
      * )
