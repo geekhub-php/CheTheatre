dc33d8816ebe8803e140351d3abc9a9fe1d3459b
Vladimir Gavrylov <iboved@gmail.com>
2015-02-26 19:42:56 +0200

    Added PostsResponse

---
+++ src/AppBundle/Model/PostsResponse.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Model;
+
+use AppBundle\Model\AbstractPaginatedModel;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * Class PostsResponse
+ * @package AppBundle\Model
+ * @ExclusionPolicy("all")
+ */
+class PostsResponse extends AbstractPaginatedModel
+{
+    /**
+     * @var Array[]
+     * @Type("array<AppBundle\Entity\Post>")
+     * @Expose
+     */
+    protected $posts;
+
+    /**
+     * @return mixed
+     */
+    public function getPosts()
+    {
+        return $this->posts;
+    }
+
+    /**
+     * @param mixed $posts
+     */
+    public function setPosts($posts)
+    {
+        $this->posts = $posts;
+    }
+}
