6df6bef3579a0e4eeb6747ad70771ac46653306f
Vladimir Gavrylov <iboved@gmail.com>
2015-03-02 02:40:52 +0200

    Added findBySlugAndDateRange method

--- src/AppBundle/Repository/PerformanceEventRepository.php
+++ src/AppBundle/Repository/PerformanceEventRepository.php
@@ -20,4 +20,22 @@ class PerformanceEventRepository extends EntityRepository
 
         return $query->execute();
     }
+
+    public function findBySlugAndDateRange(\DateTime $fromDate, \DateTime $toDate, $slug)
+    {
+        $qb = $this->createQueryBuilder('u');
+
+        $qb
+            ->join('u.performance', 'p')
+            ->WHERE('u.dateTime BETWEEN :from AND :to')
+            ->andWhere('p.slug = :slug')
+            ->setParameter('from', $fromDate->format('Y-m-d H:i'))
+            ->setParameter('to', $toDate->format('Y-m-d H:i'))
+            ->setParameter('slug', $slug)
+            ->orderBy('u.dateTime', 'ASC');
+
+        $query = $qb->getQuery();
+
+        return $query->execute();
+    }
 }
