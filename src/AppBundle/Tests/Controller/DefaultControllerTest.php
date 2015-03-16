ab6e1e9a6827026b73e401ee5e4fdbdd3fc9746a
Valik Grinevich <valik.v1per@gmail.com>
2015-03-16 11:09:02 +0200

    add test for secured dashboard ticket: https://app.asana.com/0/25002076043610/29278146417214

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,11 +2,19 @@
 
 namespace AppBundle\Tests\Controller;
 
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
 class DefaultControllerTest extends AbstractController
 {
     public function testGet()
     {
         $this->request('/');
         $this->request('/'.base_convert(md5(uniqid()), 11, 10), 'GET', 404);
+    }
+
+    public function testDashboardAccess()
+    {
+        $this->request('/dashboard', 'GET', 401);
     }
 }
