533ad35c4b4464789841360454b4fc304b345617
Valik Grinevich <valik.v1per@gmail.com>
2015-03-18 14:05:15 +0200

    require LiipMonitorBundle and enable him in AppKernel ticket: https://app.asana.com/0/25002076043610/29172655422207

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -36,6 +36,7 @@ class AppKernel extends Kernel
             new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new \GeekHub\DomainRoutingBundle\DomainRoutingBundle(),
+            new Liip\MonitorBundle\LiipMonitorBundle(),
         );
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
