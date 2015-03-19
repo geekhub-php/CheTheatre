6fda31834355f3459e949aa3c7950880878ba834
Vladimir Gavrylov <iboved@gmail.com>
2015-03-19 15:35:57 +0200

    Added PaginationLinks

---
+++ src/AppBundle/Model/PaginationLinks.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace AppBundle\Model;
+
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\Type;
+
+/**
+ * @ExclusionPolicy("all")
+ */
+class PaginationLinks
+{
+    /**
+     * @Expose()
+     * @Type("AppBundle\Model\Link")
+     */
+    protected $self;
+
+    /**
+     * @Expose()
+     * @Type("AppBundle\Model\Link")
+     */
+    protected $first;
+
+    /**
+     * @Expose()
+     * @Type("AppBundle\Model\Link")
+     */
+    protected $prev;
+
+    /**
+     * @Expose()
+     * @Type("AppBundle\Model\Link")
+     */
+    protected $next;
+
+    /**
+     * @Expose()
+     * @Type("AppBundle\Model\Link")
+     */
+    protected $last;
+
+    /**
+     * @return mixed
+     */
+    public function getSelf()
+    {
+        return $this->self;
+    }
+
+    /**
+     * @param $self
+     * @return $this
+     */
+    public function setSelf($self)
+    {
+        $this->self = $self;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirst()
+    {
+        return $this->first;
+    }
+
+    /**
+     * @param $first
+     * @return $this
+     */
+    public function setFirst($first)
+    {
+        $this->first = $first;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrev()
+    {
+        return $this->prev;
+    }
+
+    /**
+     * @param $prev
+     * @return $this
+     */
+    public function setPrev($prev)
+    {
+        $this->prev = $prev;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNext()
+    {
+        return $this->next;
+    }
+
+    /**
+     * @param $next
+     * @return $this
+     */
+    public function setNext($next)
+    {
+        $this->next = $next;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLast()
+    {
+        return $this->last;
+    }
+
+    /**
+     * @param $last
+     * @return $this
+     */
+    public function setLast($last)
+    {
+        $this->last = $last;
+
+        return $this;
+    }
+}
