63c8c1dc53993ed59bba62327c756d2aafc931f3
Valik Grinevich <valik.v1per@gmail.com>
2015-03-27 22:07:47 +0200

    fix some mistake in description ticket: https://app.asana.com/0/25002076043610/30266303723470

--- src/AppBundle/Controller/TagsController.php
+++ src/AppBundle/Controller/TagsController.php
@@ -21,7 +21,7 @@ class TagsController extends Controller
      *      404="Returned when tag by {slug} not found in database",
      *  },
      *  parameters={
-     *      {"name"="slug", "dataType"="string", "required"=true, "description"="Unique name for every employee"}
+     *      {"name"="slug", "dataType"="string", "required"=true, "description"="Unique name for every tags"}
      *  },
      *  output = "array<AppBundle\Entity\Tag>"
      * )
