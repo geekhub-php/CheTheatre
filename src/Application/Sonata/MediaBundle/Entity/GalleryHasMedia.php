1116621cdac599c9f4155eab6af5fac19eac83d4
mushkin-v <mushkin.v@gmail.com>
2015-04-01 12:34:31 +0300

    Adds translations for galleries

--- src/Application/Sonata/MediaBundle/Entity/GalleryHasMedia.php
+++ src/Application/Sonata/MediaBundle/Entity/GalleryHasMedia.php
@@ -4,13 +4,21 @@ namespace Application\Sonata\MediaBundle\Entity;
 
 use Sonata\MediaBundle\Entity\BaseGalleryHasMedia as BaseGalleryHasMedia;
 use Doctrine\ORM\Mapping as ORM;
+use Sonata\TranslationBundle\Model\Gedmo\TranslatableInterface;
+use Gedmo\Translatable\Translatable;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Sonata\TranslationBundle\Traits\Gedmo\PersonalTranslatable;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Table(name="media__gallery_media")
  * @ORM\Entity
+ * @Gedmo\TranslationEntity(class="Application\Sonata\MediaBundle\Entity\Translations\GalleryHasMediaTranslation")
  */
-class GalleryHasMedia extends BaseGalleryHasMedia
+class GalleryHasMedia extends BaseGalleryHasMedia implements TranslatableInterface
 {
+    use PersonalTranslatable;
+
     /**
      * @var integer
      *
@@ -23,14 +31,39 @@ class GalleryHasMedia extends BaseGalleryHasMedia
     /**
      * @var string
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Gedmo\Translatable
      */
     private $title;
 
     /**
      * @var string
      * @ORM\Column(type="text", nullable=true)
+     * @Gedmo\Translatable
      */
     private $description;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\OneToMany(
+     *     targetEntity="Application\Sonata\MediaBundle\Entity\Translations\GalleryHasMediaTranslation",
+     *     mappedBy="object",
+     *     cascade={"persist", "remove"}
+     * )
+     */
+    protected $translations;
+
+    /**
+     * Unset translations
+     *
+     * @return GalleryHasMedia
+     */
+    public  function unsetTranslations()
+    {
+        $this->translations = null;
+
+        return $this;
+    }
 
     /**
      * Get id
@@ -86,5 +119,21 @@ class GalleryHasMedia extends BaseGalleryHasMedia
         $this->description = $description;
 
         return $this;
+    }
+
+    /**
+     * @param string $locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
     }
 }
