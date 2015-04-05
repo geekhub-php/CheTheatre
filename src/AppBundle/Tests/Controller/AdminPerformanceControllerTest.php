9a6744a349795ad28bed9159eb69fb998ef72d8b
slavka-jd <miledi777@ukr.net>
2015-04-06 00:31:50 +0300

    Tests for Performance admin ticket https://app.asana.com/0/25002094486455/26655564623828

---
+++ src/AppBundle/Tests/Controller/AdminPerformanceControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+class AdminPerformanceControllerTest extends AbstractController
+{
+    public function testPerformanceListAction()
+    {
+        $this->request('/admin/Performance/list', 'GET', 302);
+        $this->request('/admin/Performance/list' . base_convert(md5(uniqid()), 11, 10), 'GET', 404);
+    }
+
+    public function testPerformanceCreateAction()
+    {
+        $this->request('/admin/Performance/create', 'GET', 302);
+        $this->request('/admin/Performance/create' . base_convert(md5(uniqid()), 11, 10), 'GET', 404);
+    }
+}
