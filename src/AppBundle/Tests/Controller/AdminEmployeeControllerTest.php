ccf21506112c376794ab016bb1f816d041401c39
slavka-jd <miledi777@ukr.net>
2015-04-06 00:29:41 +0300

    Tests foe Employee admin ticket https://app.asana.com/0/25002094486455/26655564623828

---
+++ src/AppBundle/Tests/Controller/AdminEmployeeControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AppBundle\Tests\Controller;
+
+class AdminEmployeeControllerTest extends AbstractController
+{
+    public function testEmployeeListAction()
+    {
+        $this->request('/admin/Employee/list', 'GET', 302);
+        $this->request('/admin/Employee/list' . base_convert(md5(uniqid()), 11, 10), 'GET', 404);
+    }
+
+    public function testEmployeeCreateAction()
+    {
+        $this->request('/admin/Employee/create', 'GET', 302);
+        $this->request('/admin/Employee/create' . base_convert(md5(uniqid()), 11, 10), 'GET', 404);
+    }
+}
