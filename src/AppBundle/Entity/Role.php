237b64a50b4978003700476498788fd5638ffb2d
Valik Grinevich <valik.v1per@gmail.com>
2015-04-08 18:04:57 +0300

    remove lenth for role description https://github.com/geekhub-php/CheTheatre/pull/82

--- src/AppBundle/Entity/Role.php
+++ src/AppBundle/Entity/Role.php
@@ -46,7 +46,7 @@ class Role extends AbstractPersonalTranslatable  implements TranslatableInterfac
     /**
      * @var string
      * @Gedmo\Translatable
-     * @ORM\Column(type="text", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      * @Type("string")
      * @Expose
      */
