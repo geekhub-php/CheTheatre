dc8ff03b357f0918cf353ecac5e14ebb39d8d3c2
Sergey Polischuk <spolischook@gmail.com>
2015-06-14 16:35:29 +0700

    Fixed migration

--- app/DoctrineMigrations/Version20150531150000.php
+++ app/DoctrineMigrations/Version20150531150000.php
@@ -16,33 +16,33 @@ class Version20150531150000 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $schema->getTable('employees')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('employees')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('employees')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('employees')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('employees')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('employees')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
 
-        $schema->getTable('history')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('history')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('history')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('history')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('history')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('history')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
 
-        $schema->getTable('performances')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('performances')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('performances')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('performances')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('performances')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('performances')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
 
-        $schema->getTable('performance_schedule')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('performance_schedule')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('performance_schedule')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('performance_schedule')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('performance_schedule')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('performance_schedule')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
 
-        $schema->getTable('posts')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('posts')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('posts')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('posts')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('posts')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('posts')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
 
-        $schema->getTable('roles')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('roles')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('roles')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('roles')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('roles')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('roles')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
 
-        $schema->getTable('tags')->addColumn('createdBy', 'string', ['length' => 255]);
-        $schema->getTable('tags')->addColumn('updatedBy', 'string', ['length' => 255]);
-        $schema->getTable('tags')->addColumn('deletedBy', 'string', ['length' => 255]);
+        $schema->getTable('tags')->addColumn('createdBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('tags')->addColumn('updatedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
+        $schema->getTable('tags')->addColumn('deletedBy', 'string', ['length' => 255, 'default' => NULL, 'notnull' => false]);
     }
 
     /**
