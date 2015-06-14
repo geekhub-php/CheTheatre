4e055af89eb9035864896454f1607f5ec9ed3bb6
Sergey Polischuk <spolischook@gmail.com>
2015-06-14 19:22:29 +0700

    Get Repertory that not in festival

--- src/AppBundle/Repository/PerformanceRepository.php
+++ src/AppBundle/Repository/PerformanceRepository.php
@@ -4,4 +4,14 @@ namespace AppBundle\Repository;
 
 class PerformanceRepository extends AbstractRepository
 {
+    public function getRepertoryCount()
+    {
+        $qb = $this->createQueryBuilder('u');
+        $query = $qb
+            ->select($qb->expr()->count('u'))
+            ->where('u.festival is NULL')
+            ->getQuery();
+
+        return $query->getSingleScalarResult();
+    }
 }
