095742e2f68577c09736c239c1b0f345ca868418
Sergey Polischuk <spolischook@gmail.com>
2015-06-14 19:19:53 +0700

    Fixed test name

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,7 +4,7 @@ namespace AppBundle\Tests\Controller;
 
 class DefaultControllerTest extends AbstractController
 {
-    public function testGet()
+    public function testGetHomeAction()
     {
         $this->request('/');
         $this->request('/'.base_convert(md5(uniqid()), 11, 10), 'GET', 404);
