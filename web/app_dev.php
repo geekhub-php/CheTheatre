70904e319025c161636c55c44072caf1b5c1d027
Sergey Polischuk <spolischook@gmail.com>
2015-09-21 10:16:20 +0300

    Removed cache from app_dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -17,7 +17,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+// $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../app/autoload.php';
 Debug::enable();
 
 require_once __DIR__.'/../app/AppKernel.php';
@@ -25,7 +26,7 @@ require_once __DIR__.'/../app/AppKernel.php';
 $request = Request::createFromGlobals();
 $kernel = new AppKernel('dev', true);
 $kernel->setDomain($request->getHttpHost());
-$kernel->loadClassCache();
+//$kernel->loadClassCache();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
