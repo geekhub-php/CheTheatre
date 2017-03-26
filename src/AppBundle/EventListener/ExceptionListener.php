f965d4b337d3b627528217eeb0f24ca833c09982
Vladimir Nimchyn <nima.vova@gmail.com>
2017-03-26 21:03:29 +0300

    add ExceptionListener

---
+++ src/AppBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\DependencyInjection\Container;
+
+class ExceptionListener
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container  $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param GetResponseForExceptionEvent $event
+     *
+     * @return JsonResponse
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $request = $this->container->get('request');
+        $route = $request->get('_route');
+        $exception = $event->getException();
+
+        $data = array(
+            'error' => array(
+                'code' => 403,
+               'message' => 'Forbidden. You don\'t have necessary permissions for the resource',
+            ),
+        );
+
+        if ($exception->getMessage() == 'Full authentication is required to access this resource.') {
+            if ($route == 'tickets' || $route == 'orders') {
+                $response = new JsonResponse($data);
+                $event->setResponse($response);
+            }
+        }
+    }
+}
