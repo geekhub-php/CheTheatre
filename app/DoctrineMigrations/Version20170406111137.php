cbb3895f818a175a7159546eb78aec1cfe080e1e
Ihor Sobchenko <sobchenko.ihor@gmail.com>
2017-04-06 15:12:46 +0300

    Added migration according actual entity stage

---
+++ app/DoctrineMigrations/Version20170406111137.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170406111137 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ticket RENAME INDEX idx_97a0ada31c7c4aad TO IDX_97A0ADA3A15A2E17');
+        $this->addSql('ALTER TABLE customer_order ADD customer_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE customer_order ADD CONSTRAINT FK_3B1CE6A39395C3F3 FOREIGN KEY (customer_id) REFERENCES customers (id) ON DELETE SET NULL');
+        $this->addSql('CREATE INDEX IDX_3B1CE6A39395C3F3 ON customer_order (customer_id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE customer_order DROP FOREIGN KEY FK_3B1CE6A39395C3F3');
+        $this->addSql('DROP INDEX IDX_3B1CE6A39395C3F3 ON customer_order');
+        $this->addSql('ALTER TABLE customer_order DROP customer_id');
+        $this->addSql('ALTER TABLE ticket RENAME INDEX idx_97a0ada3a15a2e17 TO IDX_97A0ADA31C7C4AAD');
+    }
+}
