03f1ff5cdc876125e463223d942105fd0d4ba35e
el.trubetskaya <el.trubetskaya@gmail.com>
2017-04-14 22:22:08 +0300

    change migration Version20170411152812

---
+++ app/DoctrineMigrations/Version20170411152812.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170411152812 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ticket DROP FOREIGN KEY FK_97A0ADA31C7C4AAD');
+        $this->addSql('DROP INDEX IDX_97A0ADA31C7C4AAD ON ticket');
+        $this->addSql('ALTER TABLE ticket CHANGE user_order_id user_order_id INT DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9C912ED9D ON users (api_key)');
+        $this->addSql('ALTER TABLE users RENAME INDEX uniq_62534e219be8fd98 TO UNIQ_1483A5E99BE8FD98');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ticket CHANGE user_order_id user_order_id BINARY(16) DEFAULT NULL COMMENT \'(DC2Type:uuid_binary)\'');
+        $this->addSql('ALTER TABLE ticket ADD CONSTRAINT FK_97A0ADA31C7C4AAD FOREIGN KEY (user_order_id) REFERENCES user_order (id)');
+        $this->addSql('CREATE INDEX IDX_97A0ADA31C7C4AAD ON ticket (user_order_id)');
+        $this->addSql('DROP INDEX UNIQ_1483A5E9C912ED9D ON users');
+        $this->addSql('ALTER TABLE users RENAME INDEX uniq_1483a5e99be8fd98 TO UNIQ_62534E219BE8FD98');
+    }
+}
