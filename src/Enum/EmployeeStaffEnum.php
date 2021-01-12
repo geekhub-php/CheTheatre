d5bd3de36478cc24b55a339e5df7839ed7a8c521
Sobchenko <sobchenko.ihor@gmail.com>
2021-01-12 11:08:28 +0200

    Added art-director to staff

--- src/Enum/EmployeeStaffEnum.php
+++ src/Enum/EmployeeStaffEnum.php
@@ -8,14 +8,16 @@ class EmployeeStaffEnum extends AbstractEnumType
 {
     public const ART_CORE = 'art-core';
     public const ART_PRODUCTION = 'art-production';
+    public const ART_DIRECTOR = 'art-director';
     public const ADMINISTRATIVE = 'administrative';
     public const CREATIVE_CORE = 'creative';
     public const INVITED_ACTOR = 'invited';
 
     protected static $choices = [
-        self::ART_CORE => 'Art-core',
-        self::ART_PRODUCTION => 'Art-production',
         self::ADMINISTRATIVE => 'Administrative',
+        self::ART_DIRECTOR => 'Art-director',
+        self::ART_PRODUCTION => 'Art-production',
+        self::ART_CORE => 'Art-core',
         self::CREATIVE_CORE => 'Creative',
         self::INVITED_ACTOR => 'Invited actor',
     ];
