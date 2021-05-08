f415ec8cb78e0bd0b30269b46d2c94380429026d
Serhii Polishchuk <spolischook@gmail.com>
2021-05-08 03:45:14 +0300

    Employee group admin

---
+++ src/Admin/EmployeeGroupAdmin.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Admin;
+
+use App\Entity\EmployeeGroup;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Route\RouteCollection;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class EmployeeGroupAdmin extends AbstractAdmin
+{
+    protected $baseRouteName = EmployeeGroup::class;
+    protected $baseRoutePattern = 'EmployeeGroup';
+    protected $datagridValues = [
+        '_sort_order' => 'ASC',
+        '_sort_by'    => 'position',
+    ];
+
+    /**
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     *
+     * @return void
+     */
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('title')
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     *
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('title')
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     *
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('_action', null, [
+                'actions' => [
+                    'move' => [
+                        'template' => '@PixSortableBehavior/Default/_sort.html.twig'
+                    ],
+                ],
+            ])
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     *
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+        ;
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->add('move', $this->getRouterIdParameter().'/move/{position}');
+    }
+}
