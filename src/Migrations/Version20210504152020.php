841182210b743bd538d26d33aaddda65efeddba5
Serhii Polishchuk <spolischook@gmail.com>
2021-10-19 23:41:04 +0300

    Fixed migrations order

--- src/Migrations/Version20210504152020.php
+++ src/Migrations/Version20210504152020.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20210807110835 extends AbstractMigration
+final class Version20210504152020 extends AbstractMigration
 {
     public function getDescription() : string
     {
