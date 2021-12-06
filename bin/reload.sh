de736bdc92325d44ebfa7dfd505c2c9fdb49213f
Serhii Polishchuk <spolischook@gmail.com>
2021-12-06 16:06:29 +0200

    Update dump

--- bin/reload.sh
+++ bin/reload.sh
@@ -4,5 +4,5 @@ env=${1:-dev}
 
 bin/console doctrine:database:drop --force --env=$env
 bin/console doctrine:database:create --env=$env
-bin/console doctrine:database:import 2021-11-07-Chetheatre_prod.dump.sql --env=$env
+bin/console doctrine:database:import 2021-12-06-Chetheatre_prod.dump.sql --env=$env
 bin/console doctrine:migrations:migrate --no-interaction --env=$env
